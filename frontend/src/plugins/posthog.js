d2c3ebb1464234cc160f2a530a55d0e329162180
YeapGuy <yeapguy@gmail.com>
2026-04-25 14:45:41 +0200

    add check for API key before posthog initialization

--- frontend/src/plugins/posthog.js
+++ frontend/src/plugins/posthog.js
@@ -1,9 +1,14 @@
 import posthog from "posthog-js"
-import { getCookieConsent } from "@/utils/cookie_utils"
 
 export default {
   install(Vue, options) {
-    Vue.prototype.$posthog = posthog.init(process.env.VUE_APP_POSTHOG_API_KEY, {
+    const apiKey = process.env.VUE_APP_POSTHOG_API_KEY
+    if (!apiKey) {
+      Vue.prototype.$posthog = null
+      return
+    }
+
+    Vue.prototype.$posthog = posthog.init(apiKey, {
       api_host: "https://e.timeful.app",
       capture_pageview: false,
       autocapture: false,
